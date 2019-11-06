0f6f2f161ab1e878d4f118adee22076591f459ef
shahabrar435 <57430094+shahabrar435@users.noreply.github.com>
2019-11-06 16:24:22 +0530

    Hi

---
+++ rrr/src/com/raja/Study.java
@@ -0,0 +1,18 @@
+package com.raja;
+
+
+public class Study {
+	
+	public int add(int num1,int num2)
+	{
+	   int r = num1 + num2;
+	   return r;
+	}
+	
+	public int sub(int num1,int num2)
+	{
+		int r = num1 - num2;
+		return r;
+	}
+
+}
